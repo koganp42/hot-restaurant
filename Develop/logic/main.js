348fa08e7e5aa799e7f5aa6441720956e566ef02
Kogan <koganp42@gmail.com>
2020-01-14 19:49:16 -0600

    Committing to pull

--- Develop/logic/main.js
+++ Develop/logic/main.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const reserveClass = require("./reservationClass")
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
@@ -8,9 +10,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const currentReservations = [];
-const tableList = [];
-tableList.length = 5;
 const waitingList = [];
+
+//Routes
+
+app.get("/home", function (req, res) {
+  res.send("Server works!");
+});
+
+app.get("/reserve", function (req, res) {
+  res.send("Server works!");
+});
+
+app.get("/tables", function (req, res) {
+  res.send("Server works!");
+});
+
+//Posts
+app.post("/reserve", function(req, res) {
+  let newReservation = req.body;
+  console.log("newReservation");
+  let table = new Reservation(customerName, customerEmail, customerId, phoneNumber);
+  if(currentReservations.length < 5){
+    currentReservations.push(table);
+  } else {
+    waitingList.push(table);
+  }
+  fs.appendFile("./reservations.json", JSON.stringify());
+});
+
 
 
 app.listen(PORT, function() {
