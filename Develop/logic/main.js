7b7bde21fc26085c636d0d6ecce3d175ab7518fb
Kogan <koganp42@gmail.com>
2020-01-14 20:59:10 -0600

    Commit to pull

--- Develop/logic/main.js
+++ Develop/logic/main.js
@@ -9,34 +9,70 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const currentReservations = [];
-const waitingList = [];
+// fs.readFile()
+const jsonData;
+fs.readFileSync('./logic/reservations.json', function read(err, data) {
+  if (err) {
+      throw err;
+  }
+  combinedArr = JSON.parse(data);
+  if(combinedArr === undefined){
+    return (combinedArr = [])
+  } else {
+    return combinedArr;
+  }
+});
+const currentReservations = combinedArr.currentReservations;
+const waitingList = combinedArr.waitingList;
 
 //Routes
 
 app.get("/home", function (req, res) {
-  res.send("Server works!");
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/reserve", function (req, res) {
-  res.send("Server works!");
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 app.get("/tables", function (req, res) {
-  res.send("Server works!");
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+//API JSON tables
+app.get("/api/reserve", function (req, res) {
+  return res.json(currentReservations)
 });
 
 //Posts
-app.post("/reserve", function(req, res) {
+app.post("/api/reserve", function(req, res) {
+  fs.readFileSync('./logic/reservations.json', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    combinedArr = JSON.parse(data);
+    if(combinedArr === undefined){
+      return (combinedArr = [])
+    } else {
+      return combinedArr;
+    }
+  });
+  const currentReservations = combinedArr.currentReservations;
+  const waitingList = combinedArr.waitingList;
   let newReservation = req.body;
-  console.log("newReservation");
+  console.log(newReservation);
   let table = new Reservation(customerName, customerEmail, customerId, phoneNumber);
   if(currentReservations.length < 5){
     currentReservations.push(table);
   } else {
     waitingList.push(table);
-  }
-  fs.appendFile("./reservations.json", JSON.stringify());
+  };
+  let resObj = {
+    top5: currentReservations,
+    waiting: waitingList
+  };
+  fs.writeFile("./reservations.json", JSON.stringify(resObj));
+  return res.status(204).send();
 });
 
 
